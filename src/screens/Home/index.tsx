0ddb624c88aac1d9de886a748aa13f5c226fe5f8
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-03-04 23:17:38 -0300

    feat: changed export for named and added component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from './styles'
 
-export default function Home() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Terça, 4 Novembro 2025</Text>
+      <TextInput style={styles.input} />
     </View>
   );
 }
